993d182a2b84ad61053ef6046d7d4c87076fc202
checkmateLL <saoirse.girl47@gamil.com>
2024-12-16 14:53:51 +0200

    generated html docs

--- source/conf.py
+++ source/conf.py
@@ -5,6 +5,9 @@
 
 # -- Project information -----------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
+import os
+import sys
+sys.path.append(os.path.abspath('..'))
 
 project = 'py-web-14'
 copyright = '2024, juliya'
@@ -14,7 +17,12 @@ release = '1.0'
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
-extensions = []
+
+extensions = [
+    'sphinx.ext.autodoc',
+    'sphinx.ext.napoleon',
+    'sphinx.ext.viewcode'
+]
 
 templates_path = ['_templates']
 exclude_patterns = []
@@ -24,5 +32,5 @@ exclude_patterns = []
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
-html_theme = 'alabaster'
+html_theme = 'nature'
 html_static_path = ['_static']
